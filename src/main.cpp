fe711749d9ccfdc061ec7641fc3fde6b786f035f
Benedikt Krieger <benedikt.krieger71@gmail.com>
2021-01-12 15:37:57 +0100

    added models

--- src/main.cpp
+++ src/main.cpp
@@ -225,7 +225,7 @@ private:
         createCommandPool();
         getExtensionFunctionPointers();
         createStorageImage();
-        loadModel("/viking_room.obj");
+        loadModel("/dragon.obj");
         createBottomLevelAccelerationStructure();
         createTopLevelAccelerationStructure();
         createUniformBuffer();
@@ -961,7 +961,8 @@ private:
 
         UniformBufferObject ubo{};
         glm::mat3 camRotation = glm::mat3(glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f)));
-        ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f) * camRotation, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        // ubo.view = glm::lookAt(glm::vec3(0.0f, 800.0f, 900.0f) * camRotation, glm::vec3(0.0f, 500.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        ubo.view = glm::lookAt(glm::vec3(0.75f, 0.4f, 0.75f) * camRotation, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
         ubo.proj = glm::perspective(glm::radians(45.0f), swapChainExtent.width / (float) swapChainExtent.height, 0.1f, 10.0f);
         ubo.proj[1][1] *= -1;
 
